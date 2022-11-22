6d9e515b9c7d85fe47a4bd3ebfcaff363d94af65
jcbraz <josecbraz@outlook.com>
2022-11-22 22:16:55 +0100

    Essential movement and visual layout to test it

---
+++ src/Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody
+
+export var speed := 7.0
+export var jump_strength := 20.0
+export var gravity := 50.0
+
+var _velocity := Vector3.ZERO
+var _snap_vector := Vector3.DOWN
+
+func _physics_process(delta: float) -> void:
+		var moving_direction := Vector3.ZERO
+		moving_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+		moving_direction.z = Input.get_action_strength("back") - Input.get_action_strength("foward")
+		
+		if Input.is_action_pressed("ui_right"):
+			moving_direction.x += 1
+		if Input.is_action_pressed("ui_left"):
+			moving_direction.x -= 1
+		if Input.is_action_pressed("ui_up"):
+			moving_direction.z -= 1
+		if Input.is_action_pressed("ui_down"):
+			moving_direction.z += 1
+		
+		_velocity.x = moving_direction.x * speed
+		_velocity.z = moving_direction.z * speed
+		_velocity.y -= gravity * delta
+		
+		var has_landed := is_on_floor() and _snap_vector == Vector3.ZERO
+		var is_jumping := is_on_floor() and Input.is_action_just_pressed("ui_jump")
+		if is_jumping:
+			_velocity.y = jump_strength
+			_snap_vector = Vector3.ZERO
+		elif has_landed:
+			_snap_vector = Vector3.DOWN
+		_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
+
+	
