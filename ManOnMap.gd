4f3fcc0faf8169886b98cf30599e66acfa37e7c7
sarabrownb <80540933+sarabrownb@users.noreply.github.com>
2023-01-17 17:02:21 +0100

    Add files via upload

---
+++ ManOnMap.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+
+var speed = 500
+var velocity = Vector2()
+
+onready var mapscene_GUI = get_node("../../../GUI")
+
+
+func get_input():
+	# Detect up/down/left/right keystate and only move when pressed.
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 5
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 5
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 5
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 5
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
+	
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider is StaticBody2D:
+			print("Collided with static body")
+
+
+
