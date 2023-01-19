fbd50fb28bbcac9b8798b57a6857cd2180a8c871
jcbraz <josecbraz@outlook.com>
2023-01-19 19:50:02 +0000

    Add controls and game over menu

--- src/assets/Controls.gd
+++ src/assets/Controls.gd
@@ -4,6 +4,12 @@ extends Control
 func _ready():
 	pass;
 
-func _on_GoBackButton_pressed():
-	var menu = load("res://src/assets/Menu.tscn").instance();
-	get_tree().current_scene.add_child(menu);	
+
+
+
+func _on_MainMenuButton_pressed():
+	get_tree().change_scene("res://src/assets/Menu.tscn");
+
+
+func _on_QuitGameButton_pressed():
+	get_tree().quit()
